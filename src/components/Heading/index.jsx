abdbe4be29e57f91f1d3a841d98036f588242551
houk <houk123@yandex.ru>
2025-05-01 22:07:10 +0300

    change name

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Heading = () => {
     return (
-        <h2 className="greeting">Have a nice day, Ann!</h2>
+        <h2 className="greeting">Have a nice day, Anya!</h2>
     );
 }
 
